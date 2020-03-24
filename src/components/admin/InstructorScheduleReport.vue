c39a782b28d51beee77e9b55fcfb35f31da7a531
jcmarty <59945146+jcmarty@users.noreply.github.com>
2020-03-24 18:20:15 +0800

    Revert "college sched report"
    This reverts commit 3dc5602d77c89a5cb707134a395b68b5b35dfbeb.

--- src/components/admin/InstructorScheduleReport.vue
+++ src/components/admin/InstructorScheduleReport.vue
@@ -33,8 +33,8 @@
 <script>
 import Axios from "axios";
 import {AgGridVue} from "ag-grid-vue";
-import '../../node_modules/ag-grid-community/dist/styles/ag-grid.css';
-import '../../node_modules/ag-grid-community/dist/styles/ag-theme-material.css';
+import '../../../node_modules/ag-grid-community/dist/styles/ag-grid.css';
+import '../../../node_modules/ag-grid-community/dist/styles/ag-theme-material.css';
 
     export default {
         name: 'InstructorScheduleReport',
