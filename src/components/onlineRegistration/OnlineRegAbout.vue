45c2bc4e23cfff9783a04850363476759bceeef9
Criz1019 <apidcristine@gmail.com>
2020-06-18 04:05:45 +0800

    ach

--- src/components/onlineRegistration/OnlineRegAbout.vue
+++ src/components/onlineRegistration/OnlineRegAbout.vue
@@ -25,7 +25,7 @@
 
                 <b-card id="about_card p-3" style=" box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" >
                   <img src="../../assets/images/achievement.png" img-alt="Image" id="medal" img-top to="/Achievements" />
-                    <b-link class="Stud_about">
+                    <b-link class="Stud_about" to="Achievements">
                       <center>School Achievements</center>
                     </b-link>
                 </b-card>
